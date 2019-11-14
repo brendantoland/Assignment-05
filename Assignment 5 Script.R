1b4592ac5d13fdbba9d836f008094513f3d70247
Brendan Toland <bmt223@lehigh.edu>
2019-11-14 18:56:33 -0500

    some needed changes

--- Assignment 5 Script.R
+++ Assignment 5 Script.R
@@ -1,5 +1,37 @@
+rm(list=ls(all=TRUE))
+cat("\014") 
+
+
+getwd()
+
+
+file.edit(".gitignore")
+
+
+DF <- read.csv("TotalAssetsAssignment5.csv")
+
+
+class(DF)
+typeof(DF)
+str(DF)
+
+
 if (!require("data.table")) install.packages("data.table")
 library("data.table")
+
+
+getDTthreads()
+getDTthreads(verbose=TRUE)
+setDTthreads(0)
+getDTthreads()
+
+DF <- fread("TotalAssetsAssignment5.csv", header="auto", 
+            data.table=FALSE)
+
+class(DF)
+typeof(DF)
+str(DF)
+names(DF)
 
 
 header <- read.table("TotalAssetsAssignment5.csv", header = TRUE,
